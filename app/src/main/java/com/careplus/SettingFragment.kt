fe8c338c43cb38f5148ed1df6c221959cb16a85f
Potter Hsu <potterhsu0818@gmail.com>
2019-08-05 22:05:58 +0800

    Complete setting fragment UI

--- app/src/main/java/com/careplus/SettingFragment.kt
+++ app/src/main/java/com/careplus/SettingFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,5 +17,22 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        btn_alarm.setOnClickListener {
+            layout_alarm.visibility = View.VISIBLE
+            layout_remind.visibility = View.GONE
+        }
+
+        btn_remind.setOnClickListener {
+            layout_alarm.visibility = View.GONE
+            layout_remind.visibility = View.VISIBLE
+        }
     }
 }
