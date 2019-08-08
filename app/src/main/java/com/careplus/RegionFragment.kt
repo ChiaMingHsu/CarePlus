85853f9999285aca9c292925c9c9a14bd40fb073
Potter Hsu <potterhsu0818@gmail.com>
2019-08-09 02:53:46 +0800

    Update region fragment function

--- app/src/main/java/com/careplus/RegionFragment.kt
+++ app/src/main/java/com/careplus/RegionFragment.kt
@@ -1,6 +1,8 @@
 package com.careplus
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,9 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_region.*
+import kotlinx.android.synthetic.main.fragment_region.iv_frame
 
 /**
  * A placeholder fragment containing a simple view.
@@ -30,6 +34,48 @@ class RegionFragment : Fragment() {
     }
 
     private fun setupView() {
+        btn_door.setOnClickListener {
+            with(iv_mask) {
+                if (tag == "door") {
+                    setImageDrawable(null)
+                    visibility = View.GONE
+                    tag = "none"
+                }
+                else {
+                    setImageResource(R.mipmap.region_door_mask)
+                    visibility = View.VISIBLE
+                    tag = "door"
+                }
+            }
+        }
+        btn_toilet.setOnClickListener {
+            with(iv_mask) {
+                if (tag == "toilet") {
+                    setImageDrawable(null)
+                    visibility = View.GONE
+                    tag = "none"
+                }
+                else {
+                    setImageResource(R.mipmap.region_toilet_mask)
+                    visibility = View.VISIBLE
+                    tag = "toilet"
+                }
+            }
+        }
+        btn_room.setOnClickListener {
+            with(iv_mask) {
+                if (tag == "room") {
+                    setImageDrawable(null)
+                    visibility = View.GONE
+                    tag = "none"
+                }
+                else {
+                    setImageDrawable(null)
+                    visibility = View.VISIBLE
+                    tag = "room"
+                }
+            }
+        }
         sw_door.setOnCheckedChangeListener { _, isChecked ->
             FirebaseDatabase.getInstance().getReference("settings").child(App.user.id!!).child("alarm_stuck_outdoor").setValue(isChecked)
         }
@@ -42,6 +88,18 @@ class RegionFragment : Fragment() {
     }
 
     private fun setupDB() {
+        FirebaseDatabase.getInstance().getReference("frames").child(App.user.id!!)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.child("frame").getValue(String::class.java)?.let { base64Str ->
+                        Base64.decode(base64Str, Base64.DEFAULT)
+                            .run { BitmapFactory.decodeByteArray(this, 0, this.size) }
+                            .run { iv_frame?.setImageBitmap(this) }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
         FirebaseDatabase.getInstance().getReference("settings").child(App.user.id!!)
             .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
