13a110574d9e85d78330d4b89f3b75aed09aacf3
Potter Hsu <potterhsu0818@gmail.com>
2019-08-09 01:32:27 +0800

    Complete home fragment function

--- app/src/main/java/com/careplus/HomeFragment.kt
+++ app/src/main/java/com/careplus/HomeFragment.kt
@@ -1,10 +1,17 @@
 package com.careplus
 
+import android.graphics.BitmapFactory
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,5 +23,25 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupDB()
+    }
+
+    private fun setupDB() {
+        FirebaseDatabase.getInstance().getReference("frames").child(App.user.id!!)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.child("frame").getValue(String::class.java)?.let { base64Str ->
+                        Base64.decode(base64Str, Base64.DEFAULT)
+                            .run { BitmapFactory.decodeByteArray(this, 0, this.size) }
+                            .run { iv_frame.setImageBitmap(this) }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
     }
 }
