14af34413620ccd712f8c01cab9dc8702b3741b0
Potter Hsu <potterhsu0818@gmail.com>
2019-09-17 22:12:27 +0800

    Change tab icons

--- app/src/main/java/com/careplus/HomeActivity.kt
+++ app/src/main/java/com/careplus/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.careplus
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,43 +18,53 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupView()
-        tabIndex = 0
+        tabIndex = 1
     }
 
     private fun setupView() {
-        btn_home.setOnClickListener { tabIndex = 0 }
-        btn_region.setOnClickListener { tabIndex = 1 }
-        btn_notification.setOnClickListener { tabIndex = 2 }
-        btn_setting.setOnClickListener { tabIndex = 3 }
+        btn_notification.setOnClickListener { tabIndex = 0 }
+        btn_home.setOnClickListener { tabIndex = 1 }
+        btn_setting.setOnClickListener { tabIndex = 2 }
     }
 
     private fun navigateToTab(index: Int) {
-        btn_home.setImageResource(R.mipmap.tab_home_inactive)
-        btn_region.setImageResource(R.mipmap.tab_region_inactive)
-        btn_notification.setImageResource(R.mipmap.tab_notification_inactive)
-        btn_setting.setImageResource(R.mipmap.tab_setting_inactive)
+        btn_notification.apply {
+            setImageResource(R.drawable.component_237_1)
+            setBackgroundColor(Color.rgb(248, 248, 248))
+        }
+        btn_home.apply {
+            setImageResource(R.drawable.component_241)
+            setBackgroundColor(Color.rgb(248, 248, 248))
+        }
+        btn_setting.apply {
+            setImageResource(R.drawable.component_236_1)
+            setBackgroundColor(Color.rgb(248, 248, 248))
+        }
 
         when (index) {
             0 -> {
-                btn_home.setImageResource(R.mipmap.tab_home_active)
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.layout_fragment_placeholder, HomeFragment())
-                    .commit()
-            }
-            1 -> {
-                btn_region.setImageResource(R.mipmap.tab_region_active)
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.layout_fragment_placeholder, RegionFragment())
-                    .commit()
-            }
-            2 -> {
-                btn_notification.setImageResource(R.mipmap.tab_notification_active)
+                btn_notification.apply {
+                    setImageResource(R.drawable.component_237_5)
+                    setBackgroundColor(Color.rgb(255, 255, 255))
+                }
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.layout_fragment_placeholder, NotificationFragment())
                     .commit()
             }
-            3 -> {
-                btn_setting.setImageResource(R.mipmap.tab_setting_active)
+            1 -> {
+                btn_home.apply {
+                    setImageResource(R.drawable.component_22_2)
+                    setBackgroundColor(Color.rgb(255, 255, 255))
+                }
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.layout_fragment_placeholder, HomeFragment())
+                    .commit()
+            }
+            2 -> {
+                btn_setting.apply {
+                    setImageResource(R.drawable.component_236_4)
+                    setBackgroundColor(Color.rgb(255, 255, 255))
+                }
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.layout_fragment_placeholder, SettingFragment())
                     .commit()
