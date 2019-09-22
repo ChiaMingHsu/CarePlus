d3467994ced3008c3d34e7108833de0e739378f5
Potter Hsu <potterhsu0818@gmail.com>
2019-09-23 01:51:43 +0800

    Remove redundancy

--- app/src/main/java/com/careplus/model/Message.kt
+++ app/src/main/java/com/careplus/model/Message.kt
@@ -4,8 +4,9 @@ import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class Message(
-    var type: String? = "",
-    var date: String? = "",
-    var time: String? = "",
-    var content: String? = ""
+    var type: String = "",
+    var priority: Int = 0,
+    var date: String = "",
+    var time: String = "",
+    var content: String = ""
 )
