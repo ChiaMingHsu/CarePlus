d2c1fce366dc66511f4b72704a5ca68be80667e9
Potter Hsu <potterhsu0818@gmail.com>
2019-09-25 00:28:27 +0800

    Update MessageActivity

--- app/src/main/java/com/careplus/adapters/MessageAdapter.kt
+++ app/src/main/java/com/careplus/adapters/MessageAdapter.kt
@@ -1,5 +1,6 @@
 package com.careplus.adapters
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,13 +22,27 @@ class MessageAdapter : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
     override fun getItemCount(): Int = messages.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        messages[position].let {
-//            when (it.type) {
-//                "alarm" -> holder.view.tvContent.setBackgroundResource(R.mipmap.notification_message_content_bg_alarm)
-//                "remind" -> holder.view.tvContent.setBackgroundResource(R.mipmap.notification_message_content_bg_remind)
-//            }
-            holder.view.tvDate.text = it.date
-            holder.view.tvContent.text = String.format("%s %s", it.time, it.content)
+        messages[position].let { message ->
+            when (Pair(message.type, message.priority)) {
+                Pair("alarm", "standard") -> {
+                    holder.view.ivContentBg.setImageResource(R.drawable.notification_content_alarm_standard_bg)
+                    holder.view.btnPlay.setImageResource(R.drawable.notification_btn_play_alarm_standard)
+                    holder.view.tvDate.setTextColor(Color.parseColor("#4b5cff"))
+                }
+                Pair("alarm", "emergent") -> {
+                    holder.view.ivContentBg.setImageResource(R.drawable.notification_content_alarm_emergent_bg)
+                    holder.view.btnPlay.setImageResource(R.drawable.notification_btn_play_alarm_emergent)
+                    holder.view.tvDate.setTextColor(Color.parseColor("#f77570"))
+                }
+                Pair("remind", "standard") -> {
+                    holder.view.ivContentBg.setImageResource(R.drawable.notification_content_remind_standard_bg)
+                    holder.view.btnPlay.setImageResource(R.drawable.notification_btn_play_remind_standard)
+                    holder.view.tvDate.setTextColor(Color.parseColor("#adadbe"))
+                }
+            }
+            holder.view.tvDate.text = message.date
+            holder.view.tvTime.text = message.time
+            holder.view.tvContent.text = message.content
             holder.view.btnPlay.apply {
                 tag = position
                 onBtnPlayClickListener?.run { setOnClickListener(this) }
--- app/src/main/java/com/careplus/model/Message.kt
+++ app/src/main/java/com/careplus/model/Message.kt
@@ -5,7 +5,7 @@ import com.google.firebase.database.IgnoreExtraProperties
 @IgnoreExtraProperties
 data class Message(
     var type: String = "",
-    var priority: Int = 0,
+    var priority: String = "",
     var date: String = "",
     var time: String = "",
     var content: String = ""
