9c4cdca86faaaaf1939c8b0206e7077e46108cbf
Potter Hsu <potterhsu0818@gmail.com>
2019-12-29 03:26:07 +0800

    Fix PushFragment

--- app/src/main/java/com/careplus/PushFragment.kt
+++ app/src/main/java/com/careplus/PushFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_push.*
 
 
@@ -21,11 +24,23 @@ class PushFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView()
+        setupDB()
     }
 
     private fun setupView() {
         swPush.setOnCheckedChangeListener { _, isChecked ->
             FirebaseDatabase.getInstance().getReference("settings").child(App.user.id).child("push").setValue(isChecked)
         }
+    }
+
+    private fun setupDB() {
+        FirebaseDatabase.getInstance().getReference("settings").child(App.user.id).child("push")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    swPush.isChecked = dataSnapshot.value as Boolean
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
     }
 }
